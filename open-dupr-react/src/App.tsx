e747f26920e49bb01195971664514e09159be76d
Shameer Shahab <shameershahab@gmail.com>
2025-08-09 18:14:32 -0400

    actual react setup

---
+++ open-dupr-react/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="card">
+        <Button onClick={() => setCount((count) => count + 1)}>
+          Shadcn Button count is {count}
+        </Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
